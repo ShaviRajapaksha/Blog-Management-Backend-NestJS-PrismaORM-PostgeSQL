fdc77e4a0b5d50c8d9ca5c4b15e6028d38e014a1
Shavindu Rajapaksha <shavindurajapaksha@gmail.com>
2026-01-31 21:48:43 +0530

    feat: enhance CreatePostDto with ApiProperty decorators for improved Swagger documentation

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,12 +1,25 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsInt, IsNotEmpty } from "class-validator";
 
 export class CreatePostDto {
     @IsNotEmpty()
+    @ApiProperty({
+        example: "Posting an article",
+        description: "Title",
+    })
     title: string;
 
     @IsNotEmpty()
+    @ApiProperty({
+        example: "Content of the post",
+        description: "content",
+    })    
     content: string;
 
     @IsInt()
+    @ApiProperty({
+        example: "2",
+        description: "author id",
+    })     
     authorId: number;
 }
