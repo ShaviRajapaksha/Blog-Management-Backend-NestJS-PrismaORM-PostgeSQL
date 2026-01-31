cf5914092e873a1d533322e55bc0ade09297fbb1
Shavindu Rajapaksha <shavindurajapaksha@gmail.com>
2026-01-31 15:06:21 +0530

    feat: add Users, Posts, and Comments modules to AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
+import { CommentsModule } from './comments/comments.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, UsersModule, PostsModule, CommentsModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
