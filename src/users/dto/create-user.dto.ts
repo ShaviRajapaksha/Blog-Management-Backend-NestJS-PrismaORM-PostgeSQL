a5309241b2bf5029b3f5be41d2afad1816eaf742
Shavindu Rajapaksha <shavindurajapaksha@gmail.com>
2026-01-31 15:48:14 +0530

    refactor: add ApiProperty decorators to CreateUserDto fields for better Swagger documentation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,9 +1,18 @@
 import { IsEmail, IsNotEmpty } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
 
 export class CreateUserDto {
-    @IsNotEmpty()
-    name: string;
+  @ApiProperty({
+    example: "Sam Perera",
+    description: "Full name of the user",
+  })
+  @IsNotEmpty()
+  name: string;
 
-    @IsEmail()
-    email: string;
+  @ApiProperty({
+    example: "sam@gmail.com",
+    description: "Valid email address of the user",
+  })
+  @IsEmail()
+  email: string;
 }
