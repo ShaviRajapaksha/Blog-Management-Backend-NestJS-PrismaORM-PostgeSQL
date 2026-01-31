7f6e960aac35515ca821ecd2a1ab7b202874f1ab
Shavindu Rajapaksha <shavindurajapaksha@gmail.com>
2026-01-31 21:25:47 +0530

    feat: extend UpdatePostDto with optional title, content, and published fields

--- src/posts/dto/update-post.dto.ts
+++ src/posts/dto/update-post.dto.ts
@@ -1,4 +1,15 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreatePostDto } from './create-post.dto';
+import { IsBoolean, IsOptional } from 'class-validator';
 
-export class UpdatePostDto extends PartialType(CreatePostDto) {}
+export class UpdatePostDto extends PartialType(CreatePostDto) {
+    @IsOptional()
+    title?: string | undefined;
+
+    @IsOptional()
+    content?: string | undefined;
+
+    @IsOptional()
+    @IsBoolean()
+    published?: boolean;
+}
