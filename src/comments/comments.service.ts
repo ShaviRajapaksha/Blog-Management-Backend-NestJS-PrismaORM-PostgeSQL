de168e5b5f3991bab1097d9a0a2b20879a3db04c
Shavindu Rajapaksha <shavindurajapaksha@gmail.com>
2026-01-31 15:06:09 +0530

    feat: update CommentsService to integrate Prisma for create and findAll methods

--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class CommentsService {
+  constructor(private prisma: PrismaService) {}
+
   create(createCommentDto: CreateCommentDto) {
-    return 'This action adds a new comment';
+    return this.prisma.comment.create({
+      data: createCommentDto,
+    })
   }
 
   findAll() {
-    return `This action returns all comments`;
+    return this.prisma.comment.findMany({
+      include: {user:true, post:true}
+    });
   }
 
   findOne(id: number) {
