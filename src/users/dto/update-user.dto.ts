6f112d2b16cf2724d4b61bf3be95fe2acd36594b
Shavindu Rajapaksha <shavindurajapaksha@gmail.com>
2026-01-31 21:16:54 +0530

    feat: add optional name and email fields to UpdateUserDto with validation

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,12 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
+import { IsEmail, IsOptional } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+    @IsOptional()
+    name?: string;
+
+    @IsOptional()
+    @IsEmail()
+    email?: string;
+}
