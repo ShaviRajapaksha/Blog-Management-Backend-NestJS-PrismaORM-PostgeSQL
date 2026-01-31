b6e946a8ad6e70b3a064f59749837e49f26b8d2b
Shavindu Rajapaksha <shavindurajapaksha@gmail.com>
2026-01-31 15:47:12 +0530

    refactor: reorganize imports and enhance Swagger setup in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,27 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // Enable CORS
+  app.enableCors();
 
+
+  // Swagger configuration
   const config = new DocumentBuilder()
     .setTitle('Blog API')
     .setDescription('Blog CRUD API')
     .setVersion('1.0')
     .build();
-
+  
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(3000);
+  console.log('Application is running on: http://localhost:3000');
+  console.log('Swagger documentation: http://localhost:3000/api');
 }
-
 bootstrap();
