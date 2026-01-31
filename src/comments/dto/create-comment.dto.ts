9babc688b9add94b32f31de75103a201300459ca
Shavindu Rajapaksha <shavindurajapaksha@gmail.com>
2026-01-31 13:05:55 +0530

    feat: add CreateCommentDto with validation for content, postId, and userId

--- src/comments/dto/create-comment.dto.ts
+++ src/comments/dto/create-comment.dto.ts
@@ -1,1 +1,12 @@
-export class CreateCommentDto {}
+import { IsInt, IsNotEmpty } from "class-validator";
+
+export class CreateCommentDto {
+    @IsNotEmpty()
+    content: string;
+
+    @IsInt()
+    postId: number;
+
+    @IsInt()
+    userId: number;
+}
